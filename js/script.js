1733df6a735b7708843fccefba470fec5384cfe5
CAUÃ FREIRE <cauafreire.g4@gmail.com>
2024-11-19 16:18:04 -0300

    SO FALTA O CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const aside = document.querySelector(".config")
-const qrcodeResult = document.querySelector("#qr-code img")
+const mainTag = document.querySelector(".result")
+const qrcodeResult1 = document.querySelector("#qr-code img")
+const qrcodeResult2 = document.querySelector(".result img")
 const qrcodeBtn = document.querySelector("#qr-input button")
 const qrcodeInput = document.querySelector("#qr-input input")
+const btnDownload = document.querySelector(".result a")
 console.log(qrcodeInput)
 
 const qrcodeGenerate = () => {
@@ -10,14 +13,19 @@ const qrcodeGenerate = () => {
     
     // innerTetx === innerHTML
     qrcodeBtn.innerHTML = "Gerando QR Code"
-
-    // MÉTODO DO QUERY PODEM SER PEGOS VIA PROPEIDADE (.)
+    
+    // // MÉTODO DO QUERY PODEM SER PEGOS VIA PROPEIDADE (.)
     // API
-    qrcodeResult.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
+    qrcodeResult1.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
 
-    qrcodeResult.addEventListener("load", () => {
+    // TRANSFORMAR QRCODE EM PNG
+    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(inputValue)}`;
+    qrcodeResult2.src = qrCodeUrl;
+
+    qrcodeResult1.addEventListener("load", () => {
         aside.classList.add("active")
         qrcodeBtn.innerHTML = "QR Code Gerado!" 
+        mainTag.classList.add('active')
     })
 }
 
