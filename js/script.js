bce064f0a4189a8a34640f4cec9f87da4d460a9e
CAUÃ FREIRE <cauafreire.g4@gmail.com>
2024-11-21 16:58:16 -0300

    FUNÇÃO PRONTA

--- js/script.js
+++ js/script.js
@@ -16,26 +16,41 @@ const qrcodeGenerate = () => {
     
     // // MÉTODO DO QUERY PODEM SER PEGOS VIA PROPEIDADE (.)
     // API
-    qrcodeResult1.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
+    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+    qrcodeResult1.src = qrCodeUrl;
+    qrcodeResult2.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${inputValue}`
 
-    // TRANSFORMAR QRCODE EM PNG
-    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(inputValue)}`;
-    qrcodeResult2.src = qrCodeUrl;
-
+    // REQUISIÇÃO
     qrcodeResult1.addEventListener("load", () => {
         aside.classList.add("active")
         qrcodeBtn.innerHTML = "QR Code Gerado!" 
         mainTag.classList.add('active')
+
+        // CONFIGURAR BOTÃO DE DOWNLOAD / FUNÇÃO ASSÍNCRONA
+        btnDownload.addEventListener("click", async () => {
+            const response = await fetch(qrCodeUrl) // Baixar imagem para um blob
+            const blob = await response.blob() // Converte para blob
+            const blobUrl = URL.createObjectURL(blob) // Criar uma url para blob
+
+            const link = document.createElement("a")
+            link.href = blobUrl
+            link.download = "qrcode.png"
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link)
+
+            URL.revokeObjectURL(blobUrl)
+        });
     })
 }
 
-// EVENTO BOTÃO
+// Evento do Botão
 qrcodeBtn.addEventListener("click", () => {
     qrcodeGenerate()
 })
 
-// EVENTO ENTER INPUY
-qrcodeInput.addEventListener("keydown", () => {
+// EVENTO ENTER INPUT
+qrcodeInput.addEventListener("keydown", (e) => {
     if(e.code === "Enter") {
         qrcodeGenerate()
     }
